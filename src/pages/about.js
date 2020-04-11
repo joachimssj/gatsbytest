d5aee099e45663f4d9fa832cee0a09b9e3a6dd35
joachim yazigi <joachim.yazigi@gmail.com>
2020-04-11 16:20:04 +0200

    test for deploy

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../component/header";
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Header headerText={"About Gatsby"}/>
+    <Header headerText={"cool"}/>
+    <p>Such wow. Very React.</p>
+  </div>
+)
